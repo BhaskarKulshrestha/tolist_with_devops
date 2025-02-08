2b699a2c99f03a2b13693e15fe39fea691f11e17
I528989 <bhaskar.kulshrestha@sap.com>
2025-02-08 19:43:46 +0530

    done

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server started on port 3000");
 });
+
