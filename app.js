ecc7592143012e5c998f4e6967cc6420b3e30acb
I528989 <bhaskar.kulshrestha@sap.com>
2025-02-08 19:39:26 +0530

    done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // MongoDB Connection - Use 'localhost' if MongoDB is running on the same EC2 instance
-mongoose.connect('mongodb://localhost:27017/your_database_name', {
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
